fe64b313ff15f73aabf21aef0ce287ad8caa02f4
ha <some@email.com>
2020-07-08 08:33:29 +0300

    make nameScene

--- Assets/Script/DataBasePersons.cs
+++ Assets/Script/DataBasePersons.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DataBasePersons : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public InputField ifield;
+   public Text namePerson;
+    Dictionary<string, Person> DataBase =
+    new Dictionary<string, Person>();
 
-    // Update is called once per frame
-    void Update()
+    public void nameEnter()
     {
-        
+        Debug.Log(namePerson.text);
+        bool check = false;
+        foreach (KeyValuePair<string, Person> person in DataBase)
+        {
+            if (person.Key == namePerson.text)
+            {
+                check = true;
+                Debug.Log(person.Key, person.Value);
+            }
+        }
+
+        if (check)
+        {
+
+        }
+        else
+        {
+            Person newPerson = new Person();
+            DataBase.Add(namePerson.text, newPerson);
+        }
+
+
     }
 }
--- Assets/Script/Person.cs
+++ Assets/Script/Person.cs
@@ -1,9 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Person : MonoBehaviour
 {
+    System.Random rnd = new System.Random();
+
+    private int balance;
+    
+    private List<Request> listOfRequest = new List<Request>();
+
+    public void MakeRequest()
+    {
+        Request newRequest = new Request();
+        newRequest.typeServise =rnd.Next(5);
+        listOfRequest.Add(newRequest);
+        Debug.Log(newRequest.typeServise);
+    }
+
    public void FindOutTheBalance()
     { }
 
